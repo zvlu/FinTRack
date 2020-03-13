ae82f915ab93db34cc4d3dae0b58bae54021f1a3
AndrewL <lawna12@gmail.com>
2020-03-12 21:40:37 -0400

    env variable

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@ var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + "/../config/config.js")[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (config.useEnvVariable) {
+  var sequelize = new Sequelize(process.env[config.useEnvVariable]);
 } else {
   var sequelize = new Sequelize (
     config.database,
